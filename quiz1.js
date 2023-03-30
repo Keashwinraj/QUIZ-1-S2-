8efe632e9d71262b76086a6889d4c1f04925010a
Keashwinraj <you@example.comkeashwinrajrajendran2662@gmail.com>
2023-03-30 15:59:01 +0800

    RAJ FILE(1)

---
+++ quiz1.js
@@ -0,0 +1,60 @@
+//let message = "benr"
+//console.log(message,typeof message)
+
+//message = 123
+//console.log(message,typeof message)
+
+
+//let str = "BENR"
+//let num = 3523
+
+//let course = str + num
+//console.log(course,typeof course)
+
+
+//let str = "1"
+//let num = 1
+
+//let total = parseInt(str) + num
+//console.log(total,typeof total)
+
+
+//let str = "1.258"
+//let num = 1
+
+//let total = parseFloat(str) + num
+//console.log(total,typeof total)
+
+//let data = ["Benr", 3523, true,]
+
+//console.log(data[0])
+//console.log(data[1])
+//console.log(data[2])
+
+//let user = {
+ //    name: 'RAJ',
+   //  age:23,
+    // faculty: "FKEKK"
+//}
+//console.log(user.name)
+//console.log(user.age)
+//console.log(user.faculty)
+
+let message = [
+    {
+        name: "RAJ",
+        date: "15/11/00",
+        msg: "Oh Yeah",
+        likes:"35000",
+    },
+    {
+        name: "SHARVENA",
+        date: "12/10/96",
+        msg: "WOW",
+        likes:"50000",
+    }
+
+          
+]
+
+console.log(message[0].msg)
